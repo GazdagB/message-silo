c72dbadf6e8b4e8dda090d2e047cffaa2229e844
The Code Designer <gazdagbal@gmail.com>
2024-01-24 21:24:18 +0100

    Resize Animation Blocker Implemented

--- nav.js
+++ nav.js
@@ -16,3 +16,13 @@ navToggle.addEventListener('click', function() {
 
     console.log(visibility);
 });
+
+// Resize Animation Bug Fix
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
